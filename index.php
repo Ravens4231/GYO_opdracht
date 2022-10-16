141e7a0f780e407a804f7bc4f106e87b0a14a63c
RoanvDam <114396994+RoanvdamSchool@users.noreply.github.com>
2022-10-16 21:30:21 +0200

    emojistroken voor de 5 vragen. vul in #emoji1/2/3/4/5 om zo de positie te veranderen

--- index.php
+++ index.php
@@ -1,12 +1,52 @@
 <!DOCTYPE html>
 <html lang="nl">
 <head>
+    <link href="style.css" rel="stylesheet">
     <meta charset="UTF-8">
     <title>GYO opdracht</title>
 </head>
 <body>
-
+    <!-- emojistrook voor vraag 1 -->
+    <section class="emojiStrook" id="emoji1">
+        <img src="images/angry.png" alt="angry" class="emoji" id="angry" onclick="style.border = '5px solid darkRed'">
+        <img src="images/sad.png" alt="sad" class="emoji" id="sad" onclick="style.border = '5px solid red'">
+        <img src="images/neutral.png" alt="neutraal" class="emoji" id="neutraal" onclick="style.border = '5px solid black'">
+        <img src="images/smile.png" alt="smile" class="emoji" id="smile" onclick="style.border = '5px solid lightGreen'">
+        <img src="images/veryHappy.png" alt="veryHappy" class="emoji" id="veryHappy" onclick="style.border = '5px solid green'">
+    </section>
+    <!-- emojistrook voor vraag 2 -->
+    <section class="emojiStrook" id="emoji2">
+        <img src="images/angry.png" alt="angry" class="emoji" id="angry" onclick="style.border = '5px solid darkRed'">
+        <img src="images/sad.png" alt="sad" class="emoji" id="sad" onclick="style.border = '5px solid red'">
+        <img src="images/neutral.png" alt="neutraal" class="emoji" id="neutraal" onclick="style.border = '5px solid black'">
+        <img src="images/smile.png" alt="smile" class="emoji" id="smile" onclick="style.border = '5px solid lightGreen'">
+        <img src="images/veryHappy.png" alt="veryHappy" class="emoji" id="veryHappy" onclick="style.border = '5px solid green'">
+    </section>
+    <!-- emojistrook voor vraag 3 -->
+    <section class="emojiStrook" id="emoji3">
+        <img src="images/angry.png" alt="angry" class="emoji" id="angry" onclick="style.border = '5px solid darkRed'">
+        <img src="images/sad.png" alt="sad" class="emoji" id="sad" onclick="style.border = '5px solid red'">
+        <img src="images/neutral.png" alt="neutraal" class="emoji" id="neutraal" onclick="style.border = '5px solid black'">
+        <img src="images/smile.png" alt="smile" class="emoji" id="smile" onclick="style.border = '5px solid lightGreen'">
+        <img src="images/veryHappy.png" alt="veryHappy" class="emoji" id="veryHappy" onclick="style.border = '5px solid green'">
+    </section>
+    <!-- emojistrook voor vraag 4 -->
+    <section class="emojiStrook" id="emoji4">
+        <img src="images/angry.png" alt="angry" class="emoji" id="angry" onclick="style.border = '5px solid darkRed'">
+        <img src="images/sad.png" alt="sad" class="emoji" id="sad" onclick="style.border = '5px solid red'">
+        <img src="images/neutral.png" alt="neutraal" class="emoji" id="neutraal" onclick="style.border = '5px solid black'">
+        <img src="images/smile.png" alt="smile" class="emoji" id="smile" onclick="style.border = '5px solid lightGreen'">
+        <img src="images/veryHappy.png" alt="veryHappy" class="emoji" id="veryHappy" onclick="style.border = '5px solid green'">
+    </section>
+    <!-- emojistrook voor vraag 5 -->
+    <section class="emojiStrook" id="emoji5">
+        <img src="images/angry.png" alt="angry" class="emoji" id="angry" onclick="style.border = '5px solid darkRed'">
+        <img src="images/sad.png" alt="sad" class="emoji" id="sad" onclick="style.border = '5px solid red'">
+        <img src="images/neutral.png" alt="neutraal" class="emoji" id="neutraal" onclick="style.border = '5px solid black'">
+        <img src="images/smile.png" alt="smile" class="emoji" id="smile" onclick="style.border = '5px solid lightGreen'">
+        <img src="images/veryHappy.png" alt="veryHappy" class="emoji" id="veryHappy" onclick="style.border = '5px solid green'">
+    </section>
     <?php ?>
 </body>
 </html>
-<?php
+
